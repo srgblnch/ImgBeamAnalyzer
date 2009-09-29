93c26ec95614f0c1626334a3c8afb41599d820fc
vince_soleil <vince_soleil@3b39871e-fe4b-0410-b5db-e82997f68959>
2009-09-29 17:06:53 +0000

    try reinterpret_cast cast since Yat has changed for 64bit compilation
    git-svn-id: https://svn.code.sf.net/p/tango-ds/code/DeviceClasses/Calculation/ImgBeamAnalyzer/trunk@10353 3b39871e-fe4b-0410-b5db-e82997f68959

--- src/ImgBeamAnalyzer.cpp
+++ src/ImgBeamAnalyzer.cpp
@@ -1,4 +1,4 @@
-static const char *RcsId = "$Header: /users/chaize/newsvn/cvsroot/Calculation/ImgBeamAnalyzer/src/ImgBeamAnalyzer.cpp,v 1.27 2009-09-29 15:47:46 vince_soleil Exp $";
+static const char *RcsId = "$Header: /users/chaize/newsvn/cvsroot/Calculation/ImgBeamAnalyzer/src/ImgBeamAnalyzer.cpp,v 1.28 2009-09-29 17:06:53 vince_soleil Exp $";
 //+=============================================================================
 //
 // file :         ImgBeamAnalyzer.cpp
@@ -13,7 +13,7 @@ static const char *RcsId = "$Header: /users/chaize/newsvn/cvsroot/Calculation/Im
 //
 // $Author: vince_soleil $
 //
-// $Revision: 1.27 $
+// $Revision: 1.28 $
 //
 // $Log: not supported by cvs2svn $
 //
@@ -179,10 +179,10 @@ template <> Tango::DevDouble  ImgBeamAnalyzer::DummyValue<Tango::DevDouble> ::du
 #define READ_INPUT_ATTR_WITH_ALARM( config_member_name , alarm_boolean)                         \
   {	                                                                                            \
     if (this->available_data_ == 0 || this->get_state() == Tango::FAULT)                        \
-			attr.set_value(static_cast<Tango::DevLong*>(&this->current_config_.config_member_name));                                \
+			attr.set_value(reinterpret_cast<Tango::DevLong*>(&this->current_config_.config_member_name));                                \
 		else                                                                                        \
     {                                                                                           \
-			attr.set_value(static_cast<Tango::DevLong*>(&this->available_data_->config.config_member_name));                        \
+			attr.set_value(reinterpret_cast<Tango::DevLong*>(&this->available_data_->config.config_member_name));                        \
       if (this->available_data_->alarm_boolean == true)                                         \
         attr.set_quality(Tango::ATTR_ALARM);                                                    \
       else                                                                                      \
